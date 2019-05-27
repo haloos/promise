cd5a6a7c6fb5a76075e0a8f4a2b73fc09788e3d5
Jason <haloos@me.com>
2019-05-26 23:33:06 -0400

    added async

--- promise.js
+++ promise.js
@@ -1,31 +1,6 @@
-const promise = new Promise((resolve, reject) => {
-  if (true) {
-    resolve('Stuff Worked'); 
-  } else {
-    reject('Error, it broke')
-  }
-})  
+// ASYNC AWAIT 
 
-const promise2 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 100, 'HIIII')
-})  
-const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 1000, 'POOKIE')
-}) 
-
-const promise4 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 1000, 'ARE YOU LOOKING FOR ME')
-})
-
-Promise.all([promise, promise2, promise3, promise4])
-  .then(values => {
-    console.log(values);
-  })
-
-promise 
-.then(result => result ='!') 
-.then(result2 => result2 ='?') 
-.catch(() => console.log('errror')) 
-.then(result3 => {
- console.log(result3 + '!'); 
-})
+async function fetchUsers() {
+const resp = await fetch('https://jsonplaceholder.typicode.com/users')           .then(resp => resp.json()) 
+      .then(console.log)
+}
