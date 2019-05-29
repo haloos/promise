9bb31530c7ca2bd2ab520d726d49c59c9d94cd89
Jason <haloos@me.com>
2019-05-28 21:12:59 -0400

    async example

--- promise.js
+++ promise.js
@@ -1,6 +1,7 @@
 // ASYNC AWAIT 
 
 async function fetchUsers() {
-const resp = await fetch('https://jsonplaceholder.typicode.com/users')           .then(resp => resp.json()) 
-      .then(console.log)
-}
+    const resp = await fetch('https://jsonplaceholder.typicode.com/users')    
+    const data = await resp.json(); 
+    console.log(data); 
+}       
