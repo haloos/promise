64b839e16969f032a4dc8ff66b0fd1b74fbb7184
Jason <haloos@me.com>
2019-05-28 21:44:34 -0400

    objectSpread examples

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
-// ASYNC AWAIT 
+// Object spread operator 
+const animals = {
+  tiger: 23,
+  lion: 5, 
+  monkey: 2,
+  bird: 30
+}  
 
-async function fetchUsers() {
-    const resp = await fetch('https://jsonplaceholder.typicode.com/users')    
-    const data = await resp.json(); 
-    console.log(data); 
-}       
+function objectSpread(p1, p2, p3) {
+  console.log(p1) 
+  console.log(p2) 
+  console.log(p3)
+} 
+
+const { tiger, lion, ...rest } = animals;
+
+objectSpreat(tiger, lion, rest); 
